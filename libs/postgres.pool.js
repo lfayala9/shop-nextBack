9cd2408c56cf3a9cb61503b54257daa34b7015b5
Luis Felipe <lfayala9@gmail.com>
2023-05-22 19:32:47 -0400

    deploy dev

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,16 @@
 const { Pool } =  require('pg');
-const {config } = require('../config/config')
+// const {config } = require('../config/config')
 
-const USER = encodeURIComponent(config.dbUser)
-const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+// const USER = encodeURIComponent(config.dbUser)
+// const PASSWORD = encodeURIComponent(config.dbPassword)
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 
-  const pool = new Pool({connectionString: URI});
+// const pool = new Pool({connectionString: URI});
+const pool = new Pool({
 
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+
+})
 
 module.exports = pool;
