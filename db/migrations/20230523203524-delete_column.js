c4db4452f8e7a47077dacde6e82398d9fb5ac879
Luis Felipe <lfayala9@gmail.com>
2023-05-23 16:36:59 -0400

    deploy dev

---
+++ db/migrations/20230523203524-delete_column.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { CATEGORY_TABLE } =require('./../models/category.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.removeColumn(CATEGORY_TABLE, 'image')
+  },
+
+  async down (queryInterface) {
+    await queryInterface.removeColumn(CATEGORY_TABLE, 'image')
+  }
+};
