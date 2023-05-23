0cc79354ca0d83e3d381e3b1172913907c37b202
Luis Felipe <lfayala9@gmail.com>
2023-05-23 00:26:14 -0400

    deploy dev

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const {config} = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = 'postgresql://postgres:nQx8A8qsZtEsOjk0n0mm@containers-us-west-149.railway.app:5433/railway'
 
 module.exports = {
   development: {
