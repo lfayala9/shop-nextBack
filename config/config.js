3901e7bffab5b4430a3d0e33af51f94ea8ad21b5
Luis Felipe <lfayala9@gmail.com>
2023-05-23 00:27:59 -0400

    deploy dev

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'dev',
   port: process.env.port || 3003,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
